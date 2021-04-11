c64480a3845152e4c5f96cc651574828d741e018
Chu Duong Phuong Nam <namkhmt2@gmail.com>
2021-04-11 22:08:40 +0700

    Them file tri thuc cho mn cau hinh truoc khi dung

--- DieuCheHoaHoc/MainForm.cs
+++ DieuCheHoaHoc/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,25 +17,45 @@ namespace DieuCheHoaHoc
             InitializeComponent();
         }
 
+        private bool checkFileTriThuc() {
+            if (DataTriThuc.triThucPath == null || (!File.Exists(DataTriThuc.triThucPath))) {
+                return false;
+            }
+            return true;
+        }
+
+        private void cauHinhFileTriThuc() {
+            OpenFileDialog openDlg = new OpenFileDialog();
+            openDlg.Title = "Chọn tệp chứa tri thức";
+            openDlg.InitialDirectory = @"C:\";
+            openDlg.Filter = "File văn bản (*.txt) | *.txt";
+
+            if (openDlg.ShowDialog() == DialogResult.OK) {
+                DataTriThuc.triThucPath = openDlg.FileName;
+            }
+        }
+
         private void btnDieuChe_Click(object sender, EventArgs e) {
-            if (DataTriThuc.triThucPath != null) {
+            if (checkFileTriThuc()) {
                 DieuCheForm dieuChe = new DieuCheForm();
                 this.Hide();
                 dieuChe.Show();
             }
             else {
                 MessageBox.Show("Cần chọn tệp chứa tri thức trước!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cauHinhFileTriThuc();
             }
         }
 
         private void btnThem_Click(object sender, EventArgs e) {
-            if (DataTriThuc.triThucPath != null) {
+            if (checkFileTriThuc()) {
                 ThemForm them = new ThemForm();
                 this.Hide();
                 them.Show();
             }
             else {
                 MessageBox.Show("Cần chọn tệp chứa tri thức trước!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cauHinhFileTriThuc();
             }
         }
 
@@ -44,14 +65,7 @@ namespace DieuCheHoaHoc
         }
 
         private void lblCauHinh_Click(object sender, EventArgs e) {
-            OpenFileDialog openDlg = new OpenFileDialog();
-            openDlg.Title = "Open";
-            openDlg.InitialDirectory = @"C:\Documents";
-            openDlg.Filter = "File văn bản (*.txt) | *.txt";
-
-            if (openDlg.ShowDialog() == DialogResult.OK) {
-                DataTriThuc.triThucPath = openDlg.FileName;
-            }
+            cauHinhFileTriThuc();
         }
     }
 }
